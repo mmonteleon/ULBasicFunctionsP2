686404277e91af89089c6424fa7b8680a9a1b620
EC2 Default User <ec2-user@ip-172-31-34-164.us-east-2.compute.internal>
2019-07-09 10:53:48 +0000

    added the python file

---
+++ Basic_Functions2.py
@@ -0,0 +1,22 @@
+####################################################################
+def even_or_odd(num):
+  print(str(num) + " % 2 == " + str(num%2))
+  if num % 2 == 0:
+    print(str(num) + " is even.")
+  else:
+    print(str(num) + " is odd.")
+
+# even_or_odd(5)
+# even_or_odd(6)
+
+
+
+
+def bart_chalk_writing():
+  print("Tar is not a plaything.")
+  print("Tar is not a plaything.")
+  print("Tar is not a plaything.")
+  print("Tar is not a plaything.")
+  print("Tar is not a plaything.")
+
+# bart_chalk_writing()
