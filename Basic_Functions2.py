758bf1a703f9e681b399a781c6083af577bf2de2
EC2 Default User <ec2-user@ip-172-31-34-164.us-east-2.compute.internal>
2019-07-09 10:56:18 +0000

    added the SAVED  python file

--- Basic_Functions2.py
+++ Basic_Functions2.py
@@ -1,4 +1,6 @@
 ####################################################################
+# 1 Basic functions Without Return Value
+
 def even_or_odd(num):
   print(str(num) + " % 2 == " + str(num%2))
   if num % 2 == 0:
@@ -6,11 +8,14 @@ def even_or_odd(num):
   else:
     print(str(num) + " is odd.")
 
+# Predict the output when you call the function.  Then uncomment to see
+# what happens.
+
 # even_or_odd(5)
 # even_or_odd(6)
 
-
-
+#######################################################################
+#2 Functions Without Parameters or Return Values
 
 def bart_chalk_writing():
   print("Tar is not a plaything.")
@@ -19,4 +24,12 @@ def bart_chalk_writing():
   print("Tar is not a plaything.")
   print("Tar is not a plaything.")
 
+#Predict the output when you call bart_chalk_writing().  
+# Uncomment to see what happens.
 # bart_chalk_writing()
+
+###########################################################################
+#3 
+# Buddy Programming: Write a function that prints the chorus to Old Town Road to
+# the console.
+# (It doesn't need to be that song.  Use any other song stuck in your head)
